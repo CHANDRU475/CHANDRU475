27063f756ed1e43035ad69f2a26c0112749b6ca1
chandru <chandru@chandru-Aspire-AV15-51>
2022-11-09 14:47:43 -0500

    day to seconds conversion

---
+++ addition/src/addition/second.java
@@ -0,0 +1,14 @@
+package addition;
+
+public class second {
+	
+	public static void main(String[] args) {
+		int day=12;
+		int one_day_time=24;
+		int minite=60;
+		int seconds=60;
+		int total_second=day*(one_day_time*minite*seconds);
+		System.out.println("Total seconds"+total_second+"for the "+day);
+	}
+
+}
